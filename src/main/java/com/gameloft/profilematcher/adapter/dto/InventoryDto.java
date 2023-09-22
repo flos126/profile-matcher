e81befb7fc14b9a0556eb86954c7dcf84bf0a9d1
FlorinS <sia.florin@yahoo.com>
2023-09-22 08:43:06 +0300

    Fix inventory item map deserialization, remove map name from json result

--- src/main/java/com/gameloft/profilematcher/adapter/dto/InventoryDto.java
+++ src/main/java/com/gameloft/profilematcher/adapter/dto/InventoryDto.java
@@ -1,17 +1,34 @@
 package com.gameloft.profilematcher.adapter.dto;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@Getter
+
 @Setter
 @NoArgsConstructor
 public class InventoryDto {
 
+    @JsonProperty("cash")
+    @Getter
     private int cash; // 123,
+
+    @JsonProperty("coins")
+    @Getter
     private int coins; // : 123,
-    private Map<String, Integer> items;
+
+    private Map<String, Integer> items = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Integer> getItems() {
+        return items;
+    }
+
 }
